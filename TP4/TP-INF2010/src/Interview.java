5cb0b77646824989fa247c6bef3b511cd8c6a57f
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2021-11-19 10:05:05 -0500

    TP4

---
+++ TP4/TP-INF2010/src/Interview.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Interview {
+
+        public int lastBox(int[] boxes){
+
+            // Ne pas modifier la ligne suivante
+            PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+
+            for(int box : boxes) heap.add(box);
+            while (heap.size() > 1) {
+                int boite1 = heap.poll();
+                int boite2 = heap.poll();
+                if (boite1 != boite2) {
+                    heap.add(boite1 - boite2);
+                }
+            }
+            return (heap.size() == 0) ? 0 : heap.poll();
+        }
+}
+
+
